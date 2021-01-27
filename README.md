abbe1875ea4dee3514c1bb04ef0def550e7026b4
Akeeb Saberi <akeeb.saberi@capgemini.com>
2021-01-27 12:56:23 +0000

    Search By Project
    Implemented abiliy to search by project. Only shows each user once instead of many times to ensure that there are no duplicate users appearing if they have worked on the same project more than once.
    Security testing will expose SQL injection vulnerability - to be addressed.

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Sprint 3 - Add Projects, Education and Employment to my CV
 Sprint 4 - Edit Projects, Education and Employment on my CV
 
 Sprint 5 - Delete Projects, Education and Employment from my CV
+
+Sprint 6 - Add, Edit and Delete Skills on my CV
+
+Sprint 7 - View an employee CV by searching for their name
+
+Sprint 8 - View an employee CV by searching by project
---
+++ view/ajaxsearchbyproject.php
@@ -0,0 +1,49 @@
+<?php
+
+if (isset($_POST['search'])) {
+
+  $response = "No data found!";
+
+  $servername = "localhost";
+  $database = "individual_project_db";
+  $username = "root";
+  $password = "";
+
+  $tempConnection = mysqli_connect($servername, $username, $password, $database);
+
+  $q = htmlspecialchars($_POST['q']);
+
+  $SQLquery = "SELECT u.*, p.* FROM users u INNER JOIN projects p ON u.EmployeeNumber = p.EmployeeNumber WHERE p.ProjectName LIKE '%$q%' OR p.Customer LIKE '%$q%';";
+
+  $rows = $tempConnection->query($SQLquery);
+  if ($rows && $rows->num_rows > 0) {
+
+    $arrayOfIDs = array();
+
+    $response = '<table class="table table-striped">';
+
+    foreach($rows as $row) {
+
+      $IDFromDatabase = $row["EmployeeNumber"];
+
+      if (!in_array($IDFromDatabase, $arrayOfIDs)) {
+        $response .= '<tr>';
+        $response .= '<td>' . $row["EmployeeNumber"] . '</td>';
+        $response .= '<td><a href="?page=viewuserbyusername?' . $row["EmployeeNumber"] . '">' . $row["Username"] . '</a></td>';
+        $response .= '<td>' . $row["FirstName"] . ' ' . $row["Surname"] . '</td>';
+        $response .= '<td>' . $row["ProjectName"] . '</td>';
+        $response .= '<td>' . $row["Customer"] . '</td>';
+        $response .= '</tr>';
+
+        array_push($arrayOfIDs, $IDFromDatabase);
+      }
+
+
+    }
+    $response .= "</table>";
+  }
+
+  exit($response);
+}
+
+?>
