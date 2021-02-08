765625e98097db7fef72352a339d8ccb5f6368ca
Akeeb Saberi <akeeb.saberi@capgemini.com>
2021-02-08 02:34:10 +0000

    dompdf setup
    Installed Composer to directory and installed dompdf through Composer to directory. Proof of usage through generatePDF.php file in view folder. To be worked on further in next branch.

--- view/view.php
+++ view/view.php
@@ -12,75 +12,94 @@ class View {
 
   //Display function to display a webpage
   function display() {
-    require_once("menu.php");
-    if (isset($_GET["page"])) {
-      if (strpos($_GET["page"], 'editproject') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setProjectToEdit($extractedQueryString);
-        $this->page = "view/editproject.php";
-      }
-      else if (strpos($_GET["page"], 'editeducation') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setEducationToEdit($extractedQueryString);
-        $this->page = "view/editeducation.php";
-      }
-      else if (strpos($_GET["page"], 'editusertoskill') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setUserToSkillToEdit($extractedQueryString);
-        $this->page = "view/editusertoskill.php";
-      }
-      else if (strpos($_GET["page"], 'editemployment') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setEmploymentToEdit($extractedQueryString);
-        $this->page = "view/editemployment.php";
-      }
-      else if (strpos($_GET["page"], 'deleteproject') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setProjectToDelete($extractedQueryString);
-        $this->page = "view/deleteproject.php";
-      }
-      else if (strpos($_GET["page"], 'deleteeducation') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setEducationToDelete($extractedQueryString);
-        $this->page = "view/deleteeducation.php";
-      }
-      else if (strpos($_GET["page"], 'deleteusertoskill') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setUserToSkillToDelete($extractedQueryString);
-        $this->page = "view/deleteusertoskill.php";
-      }
-      else if (strpos($_GET["page"], 'deleteemployment') !== false) {
-        $queryString = $_GET["page"];
-        $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
-        $extractedQueryString = $extractedQueryString + 0;
-        $this->model->setEmploymentToDelete($extractedQueryString);
-        $this->page = "view/deleteemployment.php";
-      }
-      else if (strpos($_GET["page"], 'viewuserbyusername') !== false) {
-        $queryString = $_GET["page"];
-        $queryString = substr($queryString, strpos($queryString, "?") + 1);
-        $queryString = $queryString + 0;
-        $this->model->setEmployeeIDToView($queryString);
-        $this->page = "view/viewuserbyusername.php";
-      }
-      else {
-        $this->page = "view/" . $_GET["page"] . ".php";
-      }
+
+    if ((isset($_GET["page"])) && (strpos($_GET["page"], 'generatePDF') !== false)) {
+      $this->page = "view/" . $_GET["page"] . ".php";
       require($this->page);
+    }
+    else if (!isset($_GET["page"])) {
+      require_once("menu.php");
+    }
+    else {
+      require_once("menu.php");
+      if (isset($_GET["page"])) {
+        if (strpos($_GET["page"], 'editproject') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setProjectToEdit($extractedQueryString);
+          $this->page = "view/editproject.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'editeducation') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setEducationToEdit($extractedQueryString);
+          $this->page = "view/editeducation.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'editusertoskill') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setUserToSkillToEdit($extractedQueryString);
+          $this->page = "view/editusertoskill.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'editemployment') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setEmploymentToEdit($extractedQueryString);
+          $this->page = "view/editemployment.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'deleteproject') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setProjectToDelete($extractedQueryString);
+          $this->page = "view/deleteproject.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'deleteeducation') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setEducationToDelete($extractedQueryString);
+          $this->page = "view/deleteeducation.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'deleteusertoskill') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setUserToSkillToDelete($extractedQueryString);
+          $this->page = "view/deleteusertoskill.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'deleteemployment') !== false) {
+          $queryString = $_GET["page"];
+          $extractedQueryString = substr($queryString, strpos($queryString, "?") + 1);
+          $extractedQueryString = $extractedQueryString + 0;
+          $this->model->setEmploymentToDelete($extractedQueryString);
+          $this->page = "view/deleteemployment.php";
+          require($this->page);
+        }
+        else if (strpos($_GET["page"], 'viewuserbyusername') !== false) {
+          $queryString = $_GET["page"];
+          $queryString = substr($queryString, strpos($queryString, "?") + 1);
+          $queryString = $queryString + 0;
+          $this->model->setEmployeeIDToView($queryString);
+          $this->page = "view/viewuserbyusername.php";
+          require($this->page);
+        }
+        else {
+          $this->page = "view/" . $_GET["page"] . ".php";
+          require($this->page);
+        }
+      }
     }
   }
 
